57461365aef70045a318fe4f0cb91fea4f064ee8
Alexis <delarosauberstrike@gmail.com>
2025-06-14 14:30:02 +0800

    feat: add Stacks component and tech stack data for frontend technologies

---
+++ src/app/components/Stacks.jsx
@@ -0,0 +1,50 @@
+"use client"
+
+import { useState } from "react";
+import stack from "../stack.js"
+
+export default function Stacks() {
+  const [stacks, setStacks] = useState(getTechStack);
+
+  function getTechStack(selectedStack) {
+    selectedStack = 'frontend'
+    const array = stack.tech_stack.frontend
+    console.log('hi',array)
+
+    const retStack = array.map((tech) => {
+      console.log(tech[0])
+      return (
+        <div
+          key={tech[0]}
+          className="block max-w-sm h-32 bg-white border border-gray-200 rounded-lg shadow-sm hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
+        >
+          <img className="w-16 h-16 object-contain mx-auto mt-4" src={tech[1]}></img>
+          <p>{tech[0]}</p>
+        </div>
+      )
+    })
+
+    return retStack;
+  }
+
+  // getTechStack()
+  return (
+   <section id="stacks" className="min-h-screen px-4 flex flex-col">
+    <div className="mb-8">
+        <h2 className="text-2xl font-bold sm:text-2xl md:text-3xl xl:text-5xl">
+          Stacks
+        </h2>
+        <p className="text-gray-400">
+          These are the main technologies used, but always exploring and learning new ones.
+        </p>
+      </div>
+    <div>
+
+    </div>
+    <div className="text-center grid grid-cols-3 md:grid-cols-4 gap-4 flr">
+      {stacks}
+    </div>
+    
+  </section>
+  );
+}
---
+++ src/app/stack.js
@@ -0,0 +1,38 @@
+const stack = {
+  "tech_stack": {
+    "frontend": [
+      ["ReactJS","https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg"],
+      ["NextJS","https://upload.wikimedia.org/wikipedia/commons/8/8e/Nextjs-logo.svg"],
+      ["VueJS","https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Vue.js_Logo_2.svg/1200px-Vue.js_Logo_2.svg.png"],
+      ["HTML","https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/HTML5_logo_and_wordmark.svg/640px-HTML5_logo_and_wordmark.svg.png"],
+      ["CSS","https://upload.wikimedia.org/wikipedia/commons/d/d5/CSS3_logo_and_wordmark.svg"],
+      ["Bootstrap","https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Bootstrap_logo.svg/1280px-Bootstrap_logo.svg.png"],
+      ["Tailwind","https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/Tailwind_CSS_Logo.svg/640px-Tailwind_CSS_Logo.svg.png"],
+      ["Chakra","https://archive.org/download/github.com-chakra-ui-chakra-ui_-_2020-02-13_17-20-29/cover.jpg"]
+    ],
+    "backend": [
+      "ExpressJS",
+      "Go",
+      "Python"
+    ],
+    "database": [
+      "MySQL",
+      "PostgreSQL",
+      "MSSQL",
+      "MongoDB",
+      "Google Firebase"
+    ],
+    "cloud_and_tools": [
+      "AWS",
+      "Github",
+    ],
+    "scripting_and_misc": [
+      "Real Virtuality Scripting",
+      "JavaScript",
+      "C",
+      "Java"
+    ]
+  }
+}
+
+export default stack;
