3878e05f4f20d3f77304cc6896007c404b25919b
Alexis <delarosauberstrike@gmail.com>
2025-06-14 11:23:10 +0800

    feat: update metadata and structure layout for improved clarity

--- src/app/page.js
+++ src/app/page.js
@@ -4,10 +4,9 @@ import AboutMe from "./components/AboutMe";
 
 export default function Home() {
   return (
-    
-    <>
+    <header className="w-full mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
       <Navbar />
       <AboutMe />
-    </>
+    </header>
   );
 }
