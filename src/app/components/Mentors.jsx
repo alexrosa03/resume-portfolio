84fe0aa956b7017fec8a58b096e248cbdcdde7b0
Alexis <delarosauberstrike@gmail.com>
2025-06-21 11:28:52 +0800

    fix: adjust padding in Mentors component for improved readability

--- src/app/components/Mentors.jsx
+++ src/app/components/Mentors.jsx
@@ -20,7 +20,7 @@ export default function Mentors() {
             <div className="p-1">
               <p className="text-lg font-semibold mb-2 text-center">Frolyn</p>
               <p className="text-md font-semibold mb-2 text-center">Full-Stack Web Engineer</p>
-              <p className="text-gray-400 pt-2 pb-2">
+              <p className="text-gray-400 pt-2 pb-10">
                 Main developer of the PJSF website and its applications. 
                 He was my first mentor in JavaScript development and 
                 emphasized the importance of using modern frameworks. His 
@@ -39,7 +39,7 @@ export default function Mentors() {
               </footer>
             </div>
           </div>
-          <div className="relative max-w-sm bg-gray-700 rounded-lg shadow-sm">
+          <div className="relative p-2 max-w-sm bg-gray-700 rounded-lg shadow-sm">
             <svg className="w-full h-48 object-cover rounded-t-lg mb-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
               <path fillRule="evenodd" d="M4 4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2H4Zm10 5a1 1 0 0 1 1-1h3a1 1 0 1 1 0 2h-3a1 1 0 0 1-1-1Zm0 3a1 1 0 0 1 1-1h3a1 1 0 1 1 0 2h-3a1 1 0 0 1-1-1Zm0 3a1 1 0 0 1 1-1h3a1 1 0 1 1 0 2h-3a1 1 0 0 1-1-1Zm-8-5a3 3 0 1 1 6 0 3 3 0 0 1-6 0Zm1.942 4a3 3 0 0 0-2.847 2.051l-.044.133-.004.012c-.042.126-.055.167-.042.195.006.013.02.023.038.039.032.025.08.064.146.155A1 1 0 0 0 6 17h6a1 1 0 0 0 .811-.415.713.713 0 0 1 .146-.155c.019-.016.031-.026.038-.04.014-.027 0-.068-.042-.194l-.004-.012-.044-.133A3 3 0 0 0 10.059 14H7.942Z" clipRule="evenodd"/>
             </svg>
@@ -47,7 +47,7 @@ export default function Mentors() {
             <div className="p-1">
               <p className="text-lg font-semibold mb-2 text-center">Justin</p>
               <p className="text-md font-semibold mb-2 text-center">Software Engineer</p>
-              <p className="text-gray-400 pt-2 pb-2">
+              <p className="text-gray-400 pt-2 pb-10">
                 Justin has been an invaluable mentor to me during my time at 
                 Newlogic. He guided me in mastering frontend development and 
                 instilled in me strong coding practices that I still carry 
