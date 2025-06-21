4b6808711aa3285a822766aa8f0f728f2bfdade1
Alexis <delarosauberstrike@gmail.com>
2025-06-21 10:47:56 +0800

    feat: add Projects and Mentors components to Home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,10 @@
-import Image from "next/image";
 import Navbar from "./components/Navbar";
 import AboutMe from "./components/AboutMe";
 import Experience from "./components/Experience";
 import Stacks from "./components/Stacks";
 import Footer from "./components/Footer";
+import Projects from "./components/Projects";
+import Mentors from "./components/Mentors";
 
 export default function Home() {
   return (
@@ -12,6 +13,8 @@ export default function Home() {
       <AboutMe />
       <Experience />
       <Stacks />
+      <Projects />
+      <Mentors />
       <Footer/>
     </header>
   );
