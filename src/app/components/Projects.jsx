3b6fe256155ffe44aa0800cb2b43a5e39f7c018b
Alexis <delarosauberstrike@gmail.com>
2025-06-21 10:36:46 +0800

    feat: add Projects component to display project images and descriptions

---
+++ src/app/components/Projects.jsx
@@ -0,0 +1,33 @@
+const images = [
+  "/images/pjsf.jpg",
+  "/images/nexus.jpg",
+]
+
+export default function Projects(){
+  return (
+    <section id="projects" className="min-h-screen px-4 flex flex-col">
+      <div className="mb-8">
+        <h2 className="text-2xl font-bold sm:text-2xl md:text-3xl xl:text-5xl">
+          Projects
+        </h2>
+        <p className="text-gray-400">
+          Here are some of the projects I have worked on.
+        </p>
+      </div>
+      <div className="grid sm:grid-cols-1 lg:grid-cols-2 gap-12">
+        <div className="w-full bg-gray-700 rounded-lg shadow-sm">
+          <img src={images[0]} alt="Project 1" className="w-full h-64 object-cover rounded-t-lg mb-4" />
+          <div className="p-1">
+            <h3 className="text-lg font-semibold mb-2 text-center"><a href="https://www.arma-ph.net/">PJSF Website</a></h3>
+          </div>
+        </div>
+        <div className=" bg-gray-700 rounded-lg shadow-sm">
+          <img src={images[1]} alt="Project 2" className="w-full h-64 object-cover rounded-t-lg mb-4" />
+          <div className="p-1">
+            <h3 className="text-lg font-semibold mb-2 text-center"><a>PJSF Nexus</a></h3>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
